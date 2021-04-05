01c05f413ec0ae2b4c9a6b9c8bd2743bac465951
Chocogrenouille <thestral@gmx.net>
2021-04-05 16:00:43 +0200

    initial setup

---
+++ js/game.js
@@ -0,0 +1,26 @@
+class Game {
+    constructor () {
+        this.background;
+        this.player;
+        this.tea;
+        this.sun;
+        this.pan;
+        this.time;
+        this.stick;
+        this.bag;
+    }
+
+    setup () {
+
+    }
+
+    draw () {
+
+    }
+
+    preload () {
+        this.background = loadImage('../img/background/hill.png');
+    }
+}
+
+
---
+++ js/main.js
@@ -0,0 +1,17 @@
+const game = new Game;
+
+
+function setup() {
+    createCanvas(1200, 600);
+    background('white')
+}
+
+
+function draw () {
+
+}
+
+
+function preload () {
+
+}
